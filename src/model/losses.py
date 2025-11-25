Fixed bugs for mixed precision
Fixed bugs when using FSDP1 with bfloat16 and FSDP2 with float16 and bfloat16.

src/model/losses.py
@@ -101,7 +101,7 @@ class YoloDFLQFLoss(nn.Module):
                 continue
 
             M = gt_boxes.size(0)
-            gt_xywh = gt_boxes[:, 0:4]
+            gt_xywh = gt_boxes[:, 0:4].to(preds.dtype)
 
             # ---------------------------------------------------
             # VECTORIZED MATCHING (NO LOOPS)
@@ -131,7 +131,7 @@ class YoloDFLQFLoss(nn.Module):
             target_scores = torch.zeros_like(matched_pred_scores)    # (M, C)
             class_ids = gt_boxes[:, 4].long().unsqueeze(1)           # (M, 1)
 
-            target_scores.scatter_(1, class_ids, iou)                # fill IoU at GT class
+            target_scores.scatter_(1, class_ids, iou.to(target_scores.dtype))                # fill IoU at GT class
 
             cls_loss = quality_focal_loss(matched_pred_scores, target_scores)
 

src/training/train_model.py
@@ -1,7 +1,7 @@
 import os
 import torch
 from tqdm import tqdm
-from torch.cuda.amp import GradScaler
+from torch.amp import GradScaler
 try:
     from torch.distributed.fsdp.sharded_grad_scaler import ShardedGradScaler
 except ImportError:
@@ -54,7 +54,7 @@ def decode_predictions(preds, conf_threshold=0.25, top_k=100):
         
         # Combine to (M, 5) format
         predictions = torch.cat([
-            filtered_boxes,
+            filtered_boxes.float(),
             filtered_classes.unsqueeze(1).float()
         ], dim=1)
         
@@ -118,11 +118,11 @@ def train(
                 if rank == 0:
                     print("[INFO] Initialized ShardedGradScaler for FSDP float16 training")
             else:
-                scaler = GradScaler()
+                scaler = GradScaler(device)
                 if rank == 0:
                     print("[INFO] ShardedGradScaler not found, falling back to GradScaler for float16")
         else:
-            scaler = GradScaler()
+            scaler = GradScaler(device)
             if rank == 0:
                 print("[INFO] Initialized GradScaler for DDP float16 training")
     elif use_amp and precision == "bfloat16" and rank == 0: