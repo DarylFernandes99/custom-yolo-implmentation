4f4f026508fd6d958970a7286ea6b30689422933
Daryl Fernandes <daryl.fernandes50@gmail.com>
2025-11-25 23:45:21 -0500

    Added dataset download script, added batch_size as optional argument

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -1,4 +1,5 @@
 import torch
+from typing import Tuple
 from torch.utils.data import DataLoader
 from torch.utils.data.distributed import DistributedSampler
 
@@ -7,7 +8,7 @@ from src.data.collate import collate_fn
 from src.data.dataset_loader import DetectionDataset
 from src.data.transforms import get_train_transforms, get_val_transforms
 
-def get_data_loaders(train_parquet, val_parquet, train_images, val_images, batch_size, is_test: bool = False):
+def get_data_loaders(train_parquet: str, val_parquet: str, train_images: str, val_images: str, batch_size: int, is_test: bool = False, prefetch_factor: int = 2) -> Tuple[DataLoader, DataLoader]:
     num_workers = get_num_workers()
 
     train_dataset = DetectionDataset(train_parquet, train_images, get_train_transforms(), is_test)
@@ -24,24 +25,26 @@ def get_data_loaders(train_parquet, val_parquet, train_images, val_images, batch
 
     print("[INFO] Creating Train Loader...", flush=True)
     train_loader = DataLoader(
-        train_dataset,
+        dataset=train_dataset,
         batch_size=batch_size,
         shuffle=(train_sampler is None),
         sampler=train_sampler,
         num_workers=num_workers,
         pin_memory=True,
+        prefetch_factor=prefetch_factor,
         collate_fn=collate_fn,
         drop_last=True
     )
 
     print("[INFO] Creating Val Loader...", flush=True)
     val_loader = DataLoader(
-        val_dataset,
+        dataset=val_dataset,
         batch_size=batch_size,
         shuffle=False,
         sampler=val_sampler,
         num_workers=num_workers,
         pin_memory=True,
+        prefetch_factor=prefetch_factor,
         collate_fn=collate_fn
     )
 
