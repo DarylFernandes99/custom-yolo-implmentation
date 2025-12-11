2ac36ac65df71968a27ad9c276bf249b6a589fa9
Daryl Fernandes <daryl.fernandes50@gmail.com>
2025-12-10 21:06:40 -0500

    feat: Add support for resuming distributed training from checkpoints and saving model configurations.

--- src/utils/common.py
+++ src/utils/common.py
@@ -1,4 +1,6 @@
 import os
+import glob
+import json
 import torch
 
 def get_num_workers():
@@ -39,3 +41,38 @@ def get_num_threads(world_size: int):
 
     print(f"Number of threads (default): {torch.get_num_threads()}")
     return torch.get_num_threads()
+
+def get_checkpoint_config(checkpoint_path):
+    """
+    Extracts the configuration from a checkpoint file.
+    """
+    try:
+        with open(os.path.join(checkpoint_path, "model_config.json"), "r") as f:
+            model_cfg_checkpoint = json.load(f)
+    except FileNotFoundError:
+        print("[WARNING] Model config file not found in checkpoint directory")
+        raise FileNotFoundError("Model config file not found in checkpoint directory")
+    
+    return model_cfg_checkpoint
+
+def find_latest_checkpoint(checkpoint_dir, extension='*.pth'):
+    """
+    Finds the file with the latest modification time in the specified directory 
+    that matches the given extension pattern.
+    """
+    # Create the search pattern
+    search_pattern = os.path.join(checkpoint_dir, extension)
+    
+    # Get a list of all files matching the pattern
+    list_of_files = glob.glob(search_pattern)
+    
+    # Filter out directories (only keep actual files)
+    files = list(filter(os.path.isfile, list_of_files))
+
+    if not files:
+        raise FileNotFoundError(f"No checkpoint files found in directory: {checkpoint_dir}")
+
+    # Find the latest file based on modification time (getmtime)
+    latest_file = max(files, key=os.path.getmtime)
+    
+    return latest_file
